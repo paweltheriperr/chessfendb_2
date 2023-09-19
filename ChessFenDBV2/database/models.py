6c8b0b487553ca762c49b20a1f2d3762c44df214
Ja <ja@mail.com>
2023-09-19 19:33:48 +0200

    create database | panel admin

--- ChessFenDBV2/database/models.py
+++ ChessFenDBV2/database/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Fen(models.Model):
+    position_id = models.IntegerField(null=False, blank=True)
+    fen = models.TextField(blank=True, null=False)
+    user = models.CharField(max_length=50, null=False, blank=True)
+
+    added = models.DateTimeField(null=False)
+
+    class Meta:
+        verbose_name = 'Fen'
+        verbose_name_plural = 'Fen\'s'
+        ordering = ['pk', 'position_id']  # Sort by `id` then `position_id`
+
+    def __str__(self):
+        return f'<Fen: {self.fen}>'
