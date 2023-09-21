9b8bdac5cd5f7b2f1ce704658694f7e19a51dbc3
Ja <ja@mail.com>
2023-09-21 10:56:11 +0200

    fast commit

--- ChessFenDBV2/main/models.py
+++ ChessFenDBV2/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Fen(models.Model):
+    # position_id = models.IntegerField(null=False, blank=True)
+    fen = models.TextField(blank=True, null=False)
+    user = models.CharField(max_length=50, null=False, blank=True)
+    debiut_name = models.CharField(max_length=255, blank=False, null=True)
+
+    added = models.DateTimeField(null=False)
+
+    class Meta:
+        verbose_name = 'Fen'
+        verbose_name_plural = 'Fen\'s'
+        ordering = ['pk']  # Sort by `id`
+
+    def __str__(self):
+        return f'<Fen: {self.fen}>'
--- ChessFenDBV2/main/views.py
+++ ChessFenDBV2/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from .models import Fen
 
 
 def index(request):
+    yes = Fen.objects.all()
     return HttpResponse('<h1>Siur</h1>')
