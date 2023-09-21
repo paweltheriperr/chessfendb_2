351b279246ca0632452345b92225fa7866dd10c6
Ja <ja@mail.com>
2023-09-21 10:44:46 +0200

    Update settings and start writting views

--- ChessFenDBV2/database/models.py
+++ ChessFenDBV2/database/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Fen(models.Model):
-    position_id = models.IntegerField(null=False, blank=True)
+    # position_id = models.IntegerField(null=False, blank=True)
     fen = models.TextField(blank=True, null=False)
     user = models.CharField(max_length=50, null=False, blank=True)
     debiut_name = models.CharField(max_length=255, blank=False, null=True)
@@ -12,7 +12,7 @@ class Fen(models.Model):
     class Meta:
         verbose_name = 'Fen'
         verbose_name_plural = 'Fen\'s'
-        ordering = ['pk', 'position_id']  # Sort by `id` then `position_id`
+        ordering = ['pk']  # Sort by `id`
 
     def __str__(self):
         return f'<Fen: {self.fen}>'
--- ChessFenDBV2/database/tests.py
+++ ChessFenDBV2/database/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+'''
+ pattern = r'^([rnbqkpRNBQKP1-8]+\/){7}[rnbqkpRNBQKP1-8]+ [wb] (-|[KQkq]+) (-|[a-h][36]) \d+ \d+$'
+    return bool(re.match(pattern, fen))
+'''
