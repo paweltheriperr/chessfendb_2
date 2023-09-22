04f6d34e01bb7e66156d3601c7e15242079b2c7c
Ja <ja@mail.com>
2023-09-22 11:35:39 +0200

    add generisc views

--- ChessFenDBV2/main/urls.py
+++ ChessFenDBV2/main/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import index, fen_list, fen_detail
+from .views import index, FenList, FenDetail
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('fens/', fen_list, name='fens'),
-    path('fens/<int:pk>/', fen_detail, name='fen_detail'),
+    path('fens/', FenList.as_view(), name='fens'),
+    path('fens/<int:pk>/', FenDetail.as_view(), name='fen_detail'),
 ]
--- ChessFenDBV2/main/views.py
+++ ChessFenDBV2/main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, HttpResponse
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404
 
 from .models import Fen
 from .serializers import FenSerializer
 
-from rest_framework.parsers import JSONParser
+from rest_framework import generics
 
 
 def index(request):
@@ -13,43 +12,15 @@ def index(request):
     return HttpResponse('<h1>Siur</h1>')
 
 
-@csrf_exempt
-def fen_list(request):
-    if request.method == 'GET':
-        fen = Fen.objects.all()
-        serializer = FenSerializer(fen, many=True)
-
-        return JsonResponse(serializer.data, safe=False)
+class FenList(generics.ListCreateAPIView):
+    queryset = Fen.objects.all()
+    serializer_class = FenSerializer
 
 
-@csrf_exempt
-def fen_detail(request, pk):
-    try:
-        fen = Fen.objects.get(pk=pk)
+class FenDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Fen.objects.all()
+    serializer_class = FenSerializer
 
-    except Fen.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = FenSerializer(fen)
-
-        return JsonResponse(serializer.data)
-
-    if request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = FenSerializer(fen, data=data)
-
-        if serializer.is_valid():
-            serializer.save()
-
-            return JsonResponse(serializer.data)
-
-        return JsonResponse(serializer.errors, status=400)
-
-    if request.method == 'DELETE':
-        fen.delete()
-
-        return HttpResponse(status=204)
 
 def daily_task(request):
     pass
