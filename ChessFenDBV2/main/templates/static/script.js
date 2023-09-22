8328987abb3c5433160f4de7320ca39476cf1624
Tekih Rekin <tekihrekin@gmail.com>
2023-09-22 13:03:33 +0200

    dodanie komentarza jak działa script

--- ChessFenDBV2/main/templates/static/script.js
+++ ChessFenDBV2/main/templates/static/script.js
@@ -1,2 +1,4 @@
 
 var board1 = Chessboard('board1', 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
+// rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1 to jest wartość fen jak zostanie podmieniona to
+// pozycjia na szachownica się zmieni
